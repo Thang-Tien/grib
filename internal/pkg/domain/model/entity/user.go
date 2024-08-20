e08cb2f5753c0638bc2664b1cc5b51ebb1685d02
mvn-thangcao-hn <thang.cao@monstar-lab.com>
2024-08-20 17:24:26 +0700

    update user, wallet entities

--- internal/pkg/domain/model/entity/user.go
+++ internal/pkg/domain/model/entity/user.go
@@ -1,11 +1,15 @@
 package entity
 
-import "time"
+import (
+	"time"
+)
 
 type User struct {
 	BaseEntity
-	BirthDay    time.Time `gorm:"column:birthday;type:datetime"`
-	Email       string    `gorm:"column:email;type:varchar(255);not null"`
-	PhoneNumber string    `gorm:"column:phone_number;type:varchar(255);not null"`
-	Address     string    `gorm:"column:address;type:varchar(255);not null"`
+	BirthDay    time.Time  `gorm:"column:birthday;type:datetime"`
+	Email       string     `gorm:"column:email;type:varchar(255);not null"`
+	PhoneNumber string     `gorm:"column:phone_number;type:varchar(255);not null"`
+	Address     string     `gorm:"column:address;type:varchar(255);not null"`
+	Customers   []Customer ``
+	Wallet      Wallet     ``
 }
--- internal/pkg/domain/model/entity/wallet.go
+++ internal/pkg/domain/model/entity/wallet.go
@@ -2,4 +2,7 @@ package entity
 
 type Wallet struct {
 	BaseEntity
+	Balance int `gorm:"column:balance"`
+	CreditCard string `gorm:"column:credit_card;not null"`
+	LinkedBankAccount string `gorm:"linked_bank_account;not null"`
 }
